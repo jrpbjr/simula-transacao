a5d7885304449335f680e8a7091e7539dcff6c6a
jrpbjr <jrpbjr@gmail.com>
2026-02-04 19:11:50 -0300

    iniciando projeto

---
+++ src/main/java/com/jrpbjr/transacao/SimulaTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulaTransacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulaTransacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jrpbjr/transacao/SimulaTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulaTransacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
