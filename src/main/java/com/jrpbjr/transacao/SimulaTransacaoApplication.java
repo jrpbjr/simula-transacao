71c56512f8546f6fc003246a0073510cbac6c078
jrpbjr <jrpbjr@gmail.com>
2026-02-04 22:16:46 -0300

    add FeignClients

--- src/main/java/com/jrpbjr/transacao/SimulaTransacaoApplication.java
+++ src/main/java/com/jrpbjr/transacao/SimulaTransacaoApplication.java
@@ -2,8 +2,10 @@ package com.jrpbjr.transacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SimulaTransacaoApplication {
 
     public static void main(String[] args) {
